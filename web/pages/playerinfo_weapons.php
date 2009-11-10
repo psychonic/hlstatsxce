e589cd8f6e0e5a972f7f8675b17cbaad8af118a5
nshastings <nshastings@localhost>
2009-11-10 22:19:49 +0300

    [svn r1432] Use l4d zombie1 for hitbox flash on zps instead of cstrike model.

--- web/pages/playerinfo_weapons.php
+++ web/pages/playerinfo_weapons.php
@@ -528,6 +528,9 @@ For support and installation notes visit http://www.hlxcommunity.com
 					case 'insmod':
 						$weapon_data[$rowdata['smweapon']]['model'] = 'insmod1';
 						break;
+					case 'zps':
+						$weapon_data[$rowdata["smweapon"]]['model'] = 'zombie1';
+						break;
 					case 'ges':
 						$weapon_data[$rowdata['smweapon']]['model'] = 'ges-bond';
 						break;
@@ -580,6 +583,9 @@ For support and installation notes visit http://www.hlxcommunity.com
 					break;
 				case 'insmod':
 					$start_model = $insmod_models[array_rand($insmod_models)];
+					break;
+				case 'zps':
+					$start_model = 'zombie1';
 					break;
 				case 'ges':
 					$start_model = $ges_models[array_rand($ges_models)];
