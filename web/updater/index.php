7be52d1abf9379212cd5f863e2943b39b97f25d5
octodhd_hlds <octodhd_hlds@monkeyrings.com>
2009-11-15 09:59:32 +0300

    [svn r1490] Fix for updater

--- web/updater/index.php
+++ web/updater/index.php
@@ -1,4 +1,4 @@
 <?php
-	$url = isset($_SERVER['PHP_SELF']) ? $_SERVER['PHP_SELF'] : getenv('PHP_SELF');
-	header('Location: $url/hlstats.php?mode=updater');
+	$url = str_replace(array("/updater", '/index.php'), array('',''), isset($_SERVER['PHP_SELF']) ? $_SERVER['PHP_SELF'] : getenv('PHP_SELF'));
+	header("Location: $url/hlstats.php?mode=updater");
 ?>
