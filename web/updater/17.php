3861fe422f3b1ecaa4757abd948f1fd04412025e
Packhead <Packhead@localhost>
2009-11-23 22:46:30 +0300

    [svn r1534] Packhead fail is Packfail.

--- web/updater/17.php
+++ web/updater/17.php
@@ -8,7 +8,6 @@
 	$db->query("
 		UPDATE hlstats_Options SET `opttype` = '2' WHERE `keyname` = 'display_style_selector';
 		");
-	}
 	
 	$db->query("
 		UPDATE hlstats_Options SET `value` = '17' WHERE `keyname` = 'dbversion';
