700dc64ef01b9e497f7196603a3654b8ec5fb255
mattias <mattias@ryrlen.org>
2009-12-09 18:12:00 +0300

    [svn r1579] Implemented more error control in heatmap class (just to confuse msleaper ;-))
    * will now check if you have right db settings, and the required table exists in db.
    * check if you have gd, pnp, jpg, ttf support in your php module
    * check if the heatmap filedirs for generated images are writable
    
    prob some other stuff that needs to be checking, but thats it for now :P

--- heatmaps/generate.php
+++ heatmaps/generate.php
@@ -6,8 +6,13 @@ ini_set("memory_limit", "32M");
 require_once 'config.inc.php';
 require_once 'heatmap.class.php';
 
+Req::checkDB();
+Req::checkGdSupport();
+Req::checkDirsWritable();
+
 $heat = new Heatmap;
 $heat->init();
+
 
 foreach (Env::get('mapinfo') as $game => $gameconf) {
         foreach ($gameconf as $map => $data) {
