732a9af7b65aad2073d82c032dc626000c1cce45
Packhead <Packhead@localhost>
2009-12-23 00:12:23 +0300

    [svn r1608] Removed Ajax update checker and implementing file_get_contents checker instead. (#965)

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -539,4 +539,27 @@ function get_player_rank($playerdata) {
 	return $rank;
 }
 
+if (!function_exists('file_get_contents')) {
+      function file_get_contents($filename, $incpath = false, $resource_context = null)
+      {
+          if (false === $fh = fopen($filename, 'rb', $incpath)) {
+              trigger_error('file_get_contents() failed to open stream: No such file or directory', E_USER_WARNING);
+              return false;
+          }
+  
+          clearstatcache();
+          if ($fsize = @filesize($filename)) {
+              $data = fread($fh, $fsize);
+          } else {
+              $data = '';
+              while (!feof($fh)) {
+                  $data .= fread($fh, 8192);
+              }
+          }
+  
+          fclose($fh);
+          return $data;
+      }
+}
+
 ?>
