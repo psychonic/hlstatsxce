b7bcc435b48fe49d5aa06ec37884339a17f20ad0
Nicholas Hastings <nshastings@gmail.com>
2010-06-15 11:01:54 -0400

    Added some failsafes to 39.php.

--- web/updater/39.php
+++ web/updater/39.php
@@ -35,19 +35,55 @@
 		$db->query("SELECT id,count FROM hlstats_Actions WHERE game='$game' and code='owner_killedobject_obj_teleporter_exit'");
 		list($tfDisassembledExitID,$tfDisassembledExitCount) = $db->fetch_row();
 		
+		if (!$tfBuiltEntranceCount)
+			$tfBuiltEntranceCount = 0;
+		if (!$tfBuiltExitCount)
+			$tfBuiltExitCount = 0;
+		if (!$tfDestroyedEntranceCount)
+			$tfDestroyedEntranceCount = 0;
+		if (!$tfDestroyedExitCount)
+			$tfDestroyedExitCount = 0;
+		if (!$tfDisassembledEntranceCount)
+			$tfDisassembledEntranceCount = 0;
+		if (!$tfDisassembledExitCount)
+			$tfDisassembledExitCount = 0;
+			
 		// Take counts of old actions and add them to new action.
 		$db->query("UPDATE hlstats_Actions SET `count`=`count`+$tfBuiltEntranceCount+$tfBuiltExitCount WHERE `id`='$tfBuiltTeleportID'");
 		$db->query("UPDATE hlstats_Actions SET `count`=`count`+$tfDestroyedEntranceCount+$tfDestroyedExitCount WHERE `id`='$tfDestroyedTeleportID'");
 		$db->query("UPDATE hlstats_Actions SET `count`=`count`+$tfDisassembledEntranceCount+$tfDisassembledExitCount WHERE `id`='$tfDisassembledTeleportID'");
 
-		// Update Events_PlayerActions
-		$db->query("UPDATE hlstats_Events_PlayerActions SET `actionId`=$tfBuiltTeleportID WHERE `actionId` IN ($tfBuiltEntranceID,$tfBuiltExitID)");
-		$db->query("UPDATE hlstats_Events_PlayerActions SET `actionId`=$tfDestroyedTeleportID WHERE `actionId` IN ($tfDestroyedEntranceID,$tfDestroyedExitID)");
-		$db->query("UPDATE hlstats_Events_PlayerActions SET `actionId`=$tfDisassembledTeleportID WHERE `actionId` IN ($tfDisassembledEntranceID,$tfDisassembledExitID)");
-		
-		// Remove old actions
-		$db->query("DELETE FROM hlstats_Actions WHERE `id` IN ($tfBuiltEntranceID,$tfBuiltExitID,$tfDestroyedEntranceID,$tfDestroyedExitID,$tfDisassembledEntranceID,$tfDisassembledExitID)");
-			
+		// Update Events_PlayerActions and remove old actions
+		if ($tfBuiltEntranceID)
+		{
+			$db->query("UPDATE hlstats_Events_PlayerActions SET `actionId`=$tfBuiltTeleportID WHERE `actionId` = $tfBuiltEntranceID");
+			$db->query("DELETE FROM hlstats_Actions WHERE `id` = $tfBuiltEntranceID");
+		}
+		if ($tfBuiltExitID)
+		{
+			$db->query("UPDATE hlstats_Events_PlayerActions SET `actionId`=$tfBuiltTeleportID WHERE `actionId` = $tfBuiltExitID");
+			$db->query("DELETE FROM hlstats_Actions WHERE `id` = $tfBuiltExitID");
+		}
+		if ($tfDestroyedEntranceID)
+		{
+			$db->query("UPDATE hlstats_Events_PlayerActions SET `actionId`=$tfDestroyedTeleportID WHERE `actionId` = $tfDestroyedEntranceID");
+			$db->query("DELETE FROM hlstats_Actions WHERE `id` = $tfDestroyedEntranceID");
+		}
+		if ($tfDestroyedExitID)
+		{
+			$db->query("UPDATE hlstats_Events_PlayerActions SET `actionId`=$tfDestroyedTeleportID WHERE `actionId` = $tfDestroyedExitID");
+			$db->query("DELETE FROM hlstats_Actions WHERE `id` = $tfDestroyedExitID");
+		}
+		if ($tfDisassembledEntranceID)
+		{
+			$db->query("UPDATE hlstats_Events_PlayerActions SET `actionId`=$tfDisassembledTeleportID WHERE `actionId` = $tfDisassembledEntranceID");
+			$db->query("DELETE FROM hlstats_Actions WHERE `id` = $tfDisassembledEntranceID");
+		}
+		if ($tfDisassembledExitID)
+		{
+			$db->query("UPDATE hlstats_Events_PlayerActions SET `actionId`=$tfDisassembledTeleportID WHERE `actionId` = $tfDisassembledExitID");
+			$db->query("DELETE FROM hlstats_Actions WHERE `id` = $tfDisassembledExitID");
+		}			
 	}
 	
 	$db->query("UPDATE hlstats_Options SET `value` = '1.6.9' WHERE `keyname` = 'version'");
