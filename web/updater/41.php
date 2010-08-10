06611a4eba7daca07271f3437811a3d453e95294
Nicholas Hastings <nshastings@gmail.com>
2010-08-10 14:55:53 -0400

    fixed sql error in 41.php

--- web/updater/41.php
+++ web/updater/41.php
@@ -21,8 +21,8 @@
 	if ($gamestring)
 	{
 		$gamestring = '('.preg_replace('/,$/', '', $gamestring).')';
-		$db->query("UPDATE `hlstats_Actions` SET `team` = '' WHERE `team` IN ('0','1','2') AND `game` IN ($gamestring)");
-		$db->query("UPDATE `hlstats_Actions` SET `for_PlayerPlayerActions` = '0', `for_PlayerActions`='1' WHERE `code` = 'Medic_Cured_Infection' AND `game` IN ($gamestring)");
+		$db->query("UPDATE `hlstats_Actions` SET `team` = '' WHERE `team` IN ('0','1','2') AND `game` IN $gamestring");
+		$db->query("UPDATE `hlstats_Actions` SET `for_PlayerPlayerActions` = '0', `for_PlayerActions`='1' WHERE `code` = 'Medic_Cured_Infection' AND `game` IN $gamestring");
 	}
 	
 	$db->query("UPDATE hlstats_Options SET `value` = '41' WHERE `keyname` = 'dbversion'");
