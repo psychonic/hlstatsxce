73f31000a8c5003caaa7f2e6815feca1c7261f02
packhead <packhead@localhost>
2010-09-18 18:57:05 -0500

    Typo in 46.php and 48.php.

--- web/updater/46.php
+++ web/updater/46.php
@@ -200,7 +200,7 @@
 	");
 
 	$db->query("
-		INSERT IGNORE INTO `hlstats_Ranks` (`game`, `image`, `minKills`, `maxKills`, `rankName`)
+		INSERT IGNORE INTO `hlstats_Ranks` (`game`, `image`, `minKills`, `maxKills`, `rankName`) VALUES
 			('pvkii', 'recruit', '0', '49', 'Recruit'),
 			('pvkii', 'private', '50', '99', 'Private'),
 			('pvkii', 'private-first-class', '100', '199', 'Private First Class'),
