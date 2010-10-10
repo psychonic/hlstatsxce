056f6bd2a3794aa761be3da352846c68da09c5b8
Nicholas Hastings <nshastings@gmail.com>
2010-10-10 11:16:03 -0400

    Fixed bug in 57.php that would trigger error when strict mode is enabled.

--- web/updater/57.php
+++ web/updater/57.php
@@ -18,7 +18,7 @@
 	{
 		$db->query("
 			INSERT INTO `hlstats_Actions` (`game`, `code`, `reward_player`, `reward_team`, `team`, `description`, `for_PlayerActions`, `for_PlayerPlayerActions`, `for_TeamActions`, `for_WorldActions`) VALUES
-				('$game', 'dalokohs', 0, 0, '', 'Ate a Dalokohs Bar', '1', '', '', ''),
+				('$game', 'dalokohs', 0, 0, '', 'Ate a Dalokohs Bar', '1', '0', '0', '0'),
 				('$game', 'dalokohs_healself', 0, 0, '', 'Ate Dalokohs Bar for Health', '1', '0', '0', '0');
 		");
 
