fef340fced5c92b02e88c3bdf00714eea6094f87
packhead <packhead@gmail.com>
2010-10-31 22:51:14 -0500

    GameSupport: Added three new awards for TF2 (#1324)

---
+++ web/updater/59.php
@@ -0,0 +1,30 @@
+<?php
+	if ( !defined('IN_UPDATER') )
+	{
+		die('Do not access this file directly.');
+	}		
+
+	$dbversion = 59;
+	$version = "1.6.12-dev";
+
+	$tfgames = array();
+	$result = $db->query("SELECT code FROM hlstats_Games WHERE realgame = 'tf'");
+	while ($rowdata = $db->fetch_row($result))
+	{ 
+		array_push($tfgames, $db->escape($rowdata[0]));
+	}
+	
+	foreach ($tfgames as $game)
+	{
+
+		$db->query("
+			INSERT IGNORE INTO `hlstats_Awards` (`awardType`, `game`, `code`, `name`, `verb`) VALUES
+				('O','$game','hit_by_train', 'Flattened', 'deaths from train'),
+				('O','$game','headshot', 'Headache', 'headshots'),
+				('W','$game','suicide', 'Doctor Assited Suicide', 'suicides');
+		");
+	}
+	
+	$db->query("UPDATE hlstats_Options SET `value` = '$version' WHERE `keyname` = 'version'");
+	$db->query("UPDATE hlstats_Options SET `value` = '$dbversion' WHERE `keyname` = 'dbversion'");
+?>
