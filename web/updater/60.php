77aaff1a01e3b926fb6ed625e2c5d87f95120ca4
Packhead <Packhead@localhost>
2010-12-19 15:57:19 -0600

    Removed extra weapon for Headtaker in TF2 Fixed wrong image name for Mad Milk Used Octo-ized names for award images
    Updater specific:
    Resolved weapon counts not being added correctly.
    Fixed stupid ENUMs that Psychonic keeps saying he is going to fix and doesn't...
    Added IGNORE parameter to INSERTs.

--- web/updater/60.php
+++ web/updater/60.php
@@ -8,7 +8,7 @@
 	$version = "1.6.12";
 
 	$insgames = array();
-	$result = $db->query("SELET code FROM hlstats_Games WHERE realgame = 'insmod'");
+	$result = $db->query("SELECT code FROM hlstats_Games WHERE realgame = 'insmod'");
 	while ($rowdata = $db->fetch_row($result))
 	{
 		array_push($insgames, $db->escape($rowdata[0]));
@@ -17,12 +17,12 @@
 	foreach ($insgames as $game)
 	{
 		$db->query("
-			INSERT INTO `hlstats_Actions` (`game`, `code`, `reward_player`, `reward_team`, `team`, `description`, `for_PlayerActions`, `for_PlayerPlayerActions`, `for_TeamActions`, `for_WorldActions`) VALUES		
-				('$game', 'captured_a', 0, 5, '', 'Captured Objective A', '', '', '1', ''),
-				('$game', 'captured_b', 0, 5, '', 'Captured Objective B', '', '', '1', ''),
-				('$game', 'captured_c', 0, 5, '', 'Captured Objective C', '', '', '1', ''),
-				('$game', 'captured_d', 0, 5, '', 'Captured Objective D', '', '', '1', ''),
-				('$game', 'captured_e', 0, 5, '', 'Captured Objective E', '', '', '1', '');
+			INSERT IGNORE INTO `hlstats_Actions` (`game`, `code`, `reward_player`, `reward_team`, `team`, `description`, `for_PlayerActions`, `for_PlayerPlayerActions`, `for_TeamActions`, `for_WorldActions`) VALUES		
+				('$game', 'captured_a', 0, 5, '', 'Captured Objective A', '0', '0', '1', '0'),
+				('$game', 'captured_b', 0, 5, '', 'Captured Objective B', '0', '0', '1', '0'),
+				('$game', 'captured_c', 0, 5, '', 'Captured Objective C', '0', '0', '1', '0'),
+				('$game', 'captured_d', 0, 5, '', 'Captured Objective D', '0', '0', '1', '0'),
+				('$game', 'captured_e', 0, 5, '', 'Captured Objective E', '0', '0', '1', '0');
 		");
 	}
 	
@@ -37,30 +37,29 @@
 	{
 	
 		$db->query("
-			INSERT INTO `hlstats_Actions` (`game`, `code`, `reward_player`, `reward_team`, `team`, `description`, `for_PlayerActions`, `for_PlayerPlayerActions`, `for_TeamActions`, `for_WorldActions`) VALUES
+			INSERT IGNORE INTO `hlstats_Actions` (`game`, `code`, `reward_player`, `reward_team`, `team`, `description`, `for_PlayerActions`, `for_PlayerPlayerActions`, `for_TeamActions`, `for_WorldActions`) VALUES
 				('$game', 'steak', 0, 0, '', 'Ate a Buffalo Steak Sandvich', '1', '0', '0', '0'),
 				('$game', 'madmilk', 0, 0, '', 'Mad Milk', '0', '1', '0', '0');
-		")
+		");
 		
 		$db->query("
 			INSERT IGNORE INTO `hlstats_Awards` (`awardType`, `game`, `code`, `name`, `verb`) VALUES
-				('O', '$game', 'steak', 'Buffalo Steak Sandvich', 'Buffalo Steak Sandviches eaten'),
+				('O', '$game', 'steak', 'Don''t Run, Its Just Steak', 'Buffalo Steak Sandviches eaten'),
 				('W', '$game', 'claidheamohmor', 'Claidheamohmor', 'Claidheamohmor kills'),
-				('W', '$game', 'back_scratcher', 'Back Scratcher', 'Back Scratcher kills'),
+				('W', '$game', 'back_scratcher', 'Rakes Wrecked', 'Back Scratcher kills'),
 				('W', '$game', 'boston_basher', 'Boston Basher', 'Boston Basher kills'),
-				('W', '$game', 'steel_fists', 'Fists of Steel', 'kills with the Fists of Steel'),
+				('W', '$game', 'steel_fists', 'My Fists They Are Made Of Steel!', 'kills with the Fists of Steel'),
 				('W', '$game', 'amputator', 'Amputator', 'Amputator kills'),
-				('W', '$game', 'tf_projectile_healing_bolt', 'Crusader''s Crossbow', 'Crusader''s Crossbow kills'),
-				('W', '$game', 'ullapool_caber', 'Ullapool Caber', 'Ullapool Caber kills'),
-				('W', '$game', 'lochnload', 'Loch-n-Load', 'Loch-n-Load kills'),
+				('W', '$game', 'tf_projectile_healing_bolt', 'Bolts Blown', 'Crusader''s Crossbow kills'),
+				('W', '$game', 'ullapool_caber', 'Caber''s Clubbed', 'Ullapool Caber kills'),
+				('W', '$game', 'lochnload', 'Loch-n-Load''d', 'Loch-n-Load kills'),
 				('W', '$game', 'brassbeast', 'Brass Beast', 'Brass Beast kills'),
-				('W', '$game', 'bear_claws', 'Warrior''s Spirit', 'Warrior''s Spirit kills'),
-				('W', '$game', 'candy_cane', 'Candy Cane', 'Candy Cane kills'),
-				('W', '$game', 'wrench_jag', 'Jag', 'Jag kills'),
-				('W', '$game', 'iron_curtain', 'Iron Curtain', 'Iron Curtain kills'),
+				('W', '$game', 'bear_claws', 'Clawed Candyasses', 'Warrior''s Spirit kills'),
+				('W', '$game', 'candy_cane', 'Canes Cracked', 'Candy Cane kills'),
+				('W', '$game', 'wrench_jag', 'Jag''d Off', 'Jag kills'),
+				('W', '$game', 'iron_curtain', 'In Soviet Russia Iron Curtain Owns You', 'Iron Curtain kills'),
 				('W', '$game', 'headtaker', 'Horseless Headless Horseman''s Headtaker', 'Horseless Headless Horseman''s Headtaker kills'),
-				('P', '$game', 'madmilk', 'Mad Milk', 'Mad Milk hits');
-			;
+				('P', '$game', 'madmilk', 'Creamed', 'Mad Milk hits');
 		");
 		
 		$db->query("
@@ -77,8 +76,7 @@
 				('$game', 'bear_claws', 'The Warrior''s Spirit', 2.0),
 				('$game', 'candy_cane', 'The Candy Cane', 2.0),
 				('$game', 'wrench_jag', 'The Jag', 2.0),
-				('$game', 'iron_curtain', 'The Iron Curtain', 1.0),
-				('$game', 'headtaker', 'The Horseless Headless Horseman''s Headtaker', 2.0);
+				('$game', 'iron_curtain', 'The Iron Curtain', 1.0);
 		");
 		
 		for ($ribbon_count = 1; $ribbon_count <= 3; $ribbon_count++) {
@@ -99,22 +97,22 @@
 			
 			$db->query("
 				INSERT IGNORE INTO `hlstats_Ribbons` (`awardCode`, `awardCount`, `special`, `game`, `image`, `ribbonName`) VALUES
-					('steak', $awardCount, 0, '$game', '" . $ribbon_count . "_steak.png', '$color Buffalo Steak Sandvich'),
-					('claidheamohmor', $awardCount, 0, '$game', '" . $ribbon_count . "_claidheamohmor.png', '$color Claidheamohmor'),
-					('back_scratcher', $awardCount, 0, '$game', '" . $ribbon_count . "_back_scratcher.png', '$color Back Scratcher'),
-					('boston_basher', $awardCount, 0, '$game', '" . $ribbon_count . "_boston_basher.png', '$color Boston Basher'),
-					('steel_fists', $awardCount, 0, '$game', '" . $ribbon_count . "_steel_fists.png', '$color Steel Fists'),
-					('amputator', $awardCount, 0, '$game', '" . $ribbon_count . "_amputator.png', '$color Amputator'),
-					('tf_projectile_healing_bolt', $awardCount, 0, '$game', '" . $ribbon_count . "_tf_projectile_healing_bolt.png', '$color Crusader''s Crossbow'),
-					('ullapool_caber', $awardCount, 0, '$game', '" . $ribbon_count . "_ullapool_caber.png', '$color Ullapool Caber'),
-					('lochnload', $awardCount, 0, '$game', '" . $ribbon_count . "_lochnload.png', '$color Loch-n-Load'),
-					('brassbeast', $awardCount, 0, '$game', '" . $ribbon_count . "_brassbeast.png', '$color Brass Beast'),
-					('bear_claws', $awardCount, 0, '$game', '" . $ribbon_count . "_bear_claws.png', '$color Warrior''s Spirit'),
-					('candy_cane', $awardCount, 0, '$game', '" . $ribbon_count . "_candy_cane.png', '$color Candy Cane'),
-					('wrench_jag', $awardCount, 0, '$game', '" . $ribbon_count . "_wrench_jag.png', '$color Jag'),
-					('iron_curtain', $awardCount, 0, '$game', '" . $ribbon_count . "_iron_curtain.png', '$color Iron Curtain'),
-					('headtaker', $awardCount, 0, '$game', '" . $ribbon_count . "_headtaker.png', '$color Horseless Headless Horseman''s Headtaker'),
-					('madmilk', $awardCount, 0, '$game', '" . $ribbon_count . "_mad_milk.png', '$color Mad Milk');
+					('steak', $award_count, 0, '$game', '" . $ribbon_count . "_steak.png', '$color Buffalo Steak Sandvich'),
+					('claidheamohmor', $award_count, 0, '$game', '" . $ribbon_count . "_claidheamohmor.png', '$color Claidheamohmor'),
+					('back_scratcher', $award_count, 0, '$game', '" . $ribbon_count . "_back_scratcher.png', '$color Back Scratcher'),
+					('boston_basher', $award_count, 0, '$game', '" . $ribbon_count . "_boston_basher.png', '$color Boston Basher'),
+					('steel_fists', $award_count, 0, '$game', '" . $ribbon_count . "_steel_fists.png', '$color Steel Fists'),
+					('amputator', $award_count, 0, '$game', '" . $ribbon_count . "_amputator.png', '$color Amputator'),
+					('tf_projectile_healing_bolt', $award_count, 0, '$game', '" . $ribbon_count . "_tf_projectile_healing_bolt.png', '$color Crusader''s Crossbow'),
+					('ullapool_caber', $award_count, 0, '$game', '" . $ribbon_count . "_ullapool_caber.png', '$color Ullapool Caber'),
+					('lochnload', $award_count, 0, '$game', '" . $ribbon_count . "_lochnload.png', '$color Loch-n-Load'),
+					('brassbeast', $award_count, 0, '$game', '" . $ribbon_count . "_brassbeast.png', '$color Brass Beast'),
+					('bear_claws', $award_count, 0, '$game', '" . $ribbon_count . "_bear_claws.png', '$color Warrior''s Spirit'),
+					('candy_cane', $award_count, 0, '$game', '" . $ribbon_count . "_candy_cane.png', '$color Candy Cane'),
+					('wrench_jag', $award_count, 0, '$game', '" . $ribbon_count . "_wrench_jag.png', '$color Jag'),
+					('iron_curtain', $award_count, 0, '$game', '" . $ribbon_count . "_iron_curtain.png', '$color Iron Curtain'),
+					('headtaker', $award_count, 0, '$game', '" . $ribbon_count . "_headtaker.png', '$color Horseless Headless Horseman''s Headtaker'),
+					('madmilk', $award_count, 0, '$game', '" . $ribbon_count . "_madmilk.png', '$color Mad Milk');
 			");	
 		}
 		
@@ -131,10 +129,10 @@
 			'bear_claws',
 			'candy_cane',
 			'wrench_jag',
-			'iron_curtain',
-			'headtaker'
+			'iron_curtain'
 		);
-
+		$tfservers = array();
+		
 		$result = $db->query("SELECT serverId FROM hlstats_Servers WHERE game = '$game'");
 		while ($rowdata = $db->fetch_row($result))
 		{ 
@@ -144,7 +142,7 @@
 		{
 			$serverstring = implode (',', $tfservers);
 			foreach ($weapons as $weapon) {
-				$db->query("UPDATE hlstats_Weapons SET `kills` = `kills` + (IFNULL((SELECT count(weapon) FROM hlstats_Events_Frags WHERE `weapon` = '$weapon' AND `serverId` IN ($serverstring))),0) WHERE `code` = '$weapon'");
+				$db->query("UPDATE hlstats_Weapons SET `kills` = `kills` + (IFNULL((SELECT count(weapon) FROM hlstats_Events_Frags WHERE `weapon` = '$weapon' AND `serverId` IN ($serverstring)),0)) WHERE `code` = '$weapon' AND `game` = '$game'");
 			}
 		}
 
