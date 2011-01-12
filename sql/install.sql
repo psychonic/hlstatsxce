de4fe489c485f960402b6dce1cd11d5ca1270927
Nicholas Hastings <nshastings@gmail.com>
2011-01-12 11:31:35 -0500

    Daemon: added teamkills to player record and teamkills+streak to player history.

--- sql/install.sql
+++ sql/install.sql
@@ -3715,6 +3715,7 @@ CREATE TABLE IF NOT EXISTS `hlstats_Players` (
   `skill` int(11) unsigned NOT NULL default '1000',
   `shots` int(11) unsigned NOT NULL default '0',
   `hits` int(11) unsigned NOT NULL default '0',
+  `teamkills` int(11) unsigned NOT NULL default '0',
   `fullName` varchar(128) default NULL,
   `email` varchar(64) default NULL,
   `homepage` varchar(64) default NULL,
@@ -3770,6 +3771,9 @@ CREATE TABLE IF NOT EXISTS `hlstats_Players_History` (
   `hits` int(11) unsigned NOT NULL default '0',
   `game` varchar(32) NOT NULL default '',
   `headshots` int(11) unsigned NOT NULL default '0',
+  `teamkills` int(11) unsigned NOT NULL default '0',
+  `kill_streak` int(6) NOT NULL default '0',
+  `death_streak` int(6) NOT NULL default '0',
   `skill_change` int(11) NOT NULL default '0',
   UNIQUE KEY `eventTime` (`eventTime`,`playerId`,`game`),
   KEY `playerId` (`playerId`)
