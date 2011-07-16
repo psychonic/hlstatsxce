48942097201915d82596d9695c87395bb7f603a8
Packhead <Packhead@localhost>
2011-07-15 21:41:37 -0500

    Daemon: Updated GeoLite_Import.sh to fix new release cycle for MaxMind GeoLite Database (#1438) (aubergine)

--- scripts/GeoLiteCity/GeoLite_Import.sh
+++ scripts/GeoLiteCity/GeoLite_Import.sh
@@ -34,25 +34,53 @@
 # 
 # For support and installation notes visit http://www.hlxcommunity.com
 
-# Easy import of geoLiteCity CSV Database
-# password must be given at the console :)
+# Configure the variables below
+
+# Set this value to 1 if you are running Gentoo linux, or any other linux distro where the "cal" command outputs not Sunday as the first day in every row!
+LINUX_OTHER="0" 
+
+# Login information for your MySQL server
 DBHOST="localhost"
 DBNAME=""
 DBUSER=""
 DBPASS=""
 
-# database is updated on 1st every month, so download the file from the 1st of current month
-DAT=`date +%Y%m01`
-FIL="GeoLiteCity_$DAT"
-FILE="$FIL.zip"
-rm *.csv
-[ -f $FILE ] || wget http://www.maxmind.com/download/geoip/database/GeoLiteCity_CSV/$FILE
-unzip -o $FIL
+#
+# Nothing to change below here.
+#
 
-mv $FIL/GeoLiteCity-Blocks.csv geoLiteCity_Blocks.csv
-mv $FIL/GeoLiteCity-Location.csv geoLiteCity_Location.csv.temp
-rmdir $FIL
+
+# database is updated every first tuesday of any month, so download it with that specific date and import it
+TODAY_MONTH=$( date +%m )
+TODAY_YEAR=$( date +%Y )
+if [ $LINUX_OTHER == "1" ]
+ then CAL_COMMAND="cal -s" 
+ else CAL_COMMAND="cal" 
+fi
+FIRST_TUESDAY_MONTH=$( $CAL_COMMAND $TODAY_MONTH $TODAY_YEAR |
+ awk '
+  NR == 1 { next }
+  NR == 2 { next }
+  NF <= 4 { next }
+  NF == 5 { print $1 ; exit }
+  NF == 6 { print $2 ; exit }
+  NF == 7 { print $3 ; exit }
+ ' )
+
+DATE=""$TODAY_YEAR""$TODAY_MONTH"0"$FIRST_TUESDAY_MONTH"" 
+DIR="GeoLiteCity_$DATE" 
+FILE="GeoLiteCity_$DATE.zip" 
+ls *.csv &>/dev/null && rm *.csv
+[ -f $FILE ] || wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity_CSV/$FILE || exit 1
+unzip -o $FILE || exit 1
+mv $DIR/GeoLiteCity-Blocks.csv geoLiteCity_Blocks.csv
+mv $DIR/GeoLiteCity-Location.csv geoLiteCity_Location.csv.temp
 iconv -f ISO-8859-1 -t UTF-8 geoLiteCity_Location.csv.temp > geoLiteCity_Location.csv
 mysqlimport -C -d --fields-terminated-by=, --fields-enclosed-by=\" --ignore-lines=2 --default-character-set=utf8 -L -i -h $DBHOST -u $DBUSER --password=$DBPASS $DBNAME geoLiteCity_Blocks.csv 
 mysqlimport -C -d --fields-terminated-by=, --fields-enclosed-by=\" --ignore-lines=2 --default-character-set=utf8 -L -i -h $DBHOST -u $DBUSER --password=$DBPASS $DBNAME geoLiteCity_Location.csv 
-rm *.csv
+
+# Cleanup
+ls *.csv &>/dev/null && rm *.csv
+ls *.csv.temp &>/dev/null && rm *.csv.temp
+rm $FILE
+rmdir $DIR
